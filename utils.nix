61e66c1b21bd4e005bf0e63118502e3e6a4af29e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2024-02-04 17:53:28 -0500

    add functions for constructing simple geometric objects

--- utils.nix
+++ utils.nix
@@ -16,4 +16,23 @@ rec {
   # liftPoint :: Int -> Number -> Point2D -> Point3D
   liftPoint = axis: w: p: listToPoint (insertAt axis w (pointToList p));
 
+  # TODO: make this a specific case of a more general shape class
+  # UnitSquare :: Shape
+  UnitSquare = let d = {x = 0.5; y = 0.5;}; in
+    # map (addPoints d) (lib.take 4 (iterate rot90 d));
+    map (addPoints d) (iterate rot90 4 d);
+  # Square = pos: size: (translate pos) (scale size) UnitSquare;
+
+  # Cube :: Point3D -> Number -> Geometry
+  Cube = pos: size: compose (translate pos) (scale size) {
+    # faces = map (mapPoints (liftPoint) (Square pos size)) [0 1 2];
+    position = origin;
+    faces = map ({i, j}: map (liftPoint i j) UnitSquare)
+      (lib.cartesianProductOfSets {i = [0 1 2]; j = [0 1];});
+    # faces = [UnitSquare];
+    # faces = [[origin]];
+
+    # volume = 
+  };
+  Sphere = pos: radius: { pos = pos; radius = radius; };
 }
